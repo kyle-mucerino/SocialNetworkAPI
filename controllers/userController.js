cfd34f7ad03181c149b070fd3a4d0ec63f18802e
kyle-mucerino <kyle.mucerino@gmail.com>
2023-04-14 18:14:41 -0700

    userController.js to get, create, update, find, delete, etc

--- controllers/userController.js
+++ controllers/userController.js
@@ -82,7 +82,34 @@ const userController = {
 
   addFriend(req, res) {
     User.findOneAndUpdate({ _id: req.params.userId }, {
-      
-    })
-  }
-}
+    $addToSet: { friends: req.params.friendId} }, { new:
+    true })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res.status(404).json({ message: 'No user with this id!' });
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  removeFriend(req, res) {
+    User.findOneAndUpdate({ _id: req.params.userId }, {
+    $pull: { friends: req.params.friendId } }, { new: true })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res.status(404).json({ message: 'No user with this id!' });
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+};
+
+module.exports = userController;
